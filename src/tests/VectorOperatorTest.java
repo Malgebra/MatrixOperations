6fb8601ef4010e3cabe5066ba0029bc3c0f0e93b
Emmanuel Guzman <eguzmandls@gmail.com>
2024-04-01 12:50:00 -0400

    BYE HAT

--- src/tests/VectorOperatorTest.java
+++ src/tests/VectorOperatorTest.java
@@ -1,10 +1,6 @@
 package tests;
 
-import static org.junit.Assert.*;
-
-import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 
 import vector.Vector;
 import vector.operations.VectorOperator;
