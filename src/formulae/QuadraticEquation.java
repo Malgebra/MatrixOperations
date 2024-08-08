512a68f57a9ba440a4abec8b588b2cb6a6ed349e
BS23 <eguzmandls@gmail.com>
2024-08-08 03:38:24 -0400

    Added Formuale package

---
+++ src/formulae/QuadraticEquation.java
@@ -0,0 +1,11 @@
+package formulae;
+
+public class QuadraticEquation {
+	public QuadraticEquation(double a, double b, double c) {
+		System.out.println("Solving quadratic equation from values a,b, and c.");
+		
+		double resultA;
+		double resultB;
+	}
+
+}
